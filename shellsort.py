9e0e6444de3adf577ef11126e62e60a36568d9b2
SaintSerem <Brianb.kiptoo@strathmore.edu>
2024-06-24 08:33:28 +0300

    Minor changes made.

--- shellsort.py
+++ shellsort.py
@@ -1,32 +1,27 @@
 def shellSort(arr):
-    # Start with a big gap, then reduce the gap
+    
     n = len(arr)
     gap = n // 2 
 
-    # Do a gapped insertion sort for this gap size.
-    # The first gap elements a[0..gap-1] are already in gapped
-    # order keep adding one more element until the entire array
-    # is gap sorted
+    
     while gap > 0:
         for i in range(gap, n):
-            # add a[i] to the elements that have been gap sorted
-            # save a[i] in temp and make a hole at position i
+        
             temp = arr[i]
 
-            # shift earlier gap-sorted elements up until the correct
-            # location for a[i] is found
+           
             j = i
             while j >= gap and arr[j - gap] > temp:
                 arr[j] = arr[j - gap]
                 j -= gap
 
-            # put temp (the original a[i]) in its correct location
+
             arr[j] = temp
-        gap //= 2  # Use integer division instead of float division
+        gap //= 2  # 
 
 
-# Driver code to test above
-arr = [12, 34, 54, 2, 3]
+# test code
+arr = [35, 33, 42, 10,14,19,27,44]
 
 n = len(arr)
 print("Array before sorting:")
